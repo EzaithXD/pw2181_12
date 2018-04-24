a03c895428071d044496635c209c3af1ea5e4224
German <german2506.gs@gmail.com>
2018-04-24 13:03:02 -0600

    primer jquery

---
+++ primerjquery.js
@@ -0,0 +1,34 @@
+
+
+var inicia =function(){
+
+	var nuevo = function(){
+
+
+		$.ajax({
+		  url: 'https://randomuser.me/api/',
+		  dataType: 'json',
+		  success: function(data) {
+
+
+
+		    $("#nombre").html(data.results[0].name.first+"   "+data.results[0].name.last);  
+		}
+		});
+	     
+
+
+	}
+$("#btnNuevo").on("click",nuevo);
+}
+
+
+
+
+
+
+
+
+
+
+$(document).ready(inicia);
