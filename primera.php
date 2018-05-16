f6d46a524053c7c0129a7565c89d29ad507ae64d
German <german2506.gs@gmail.com>
2018-05-16 12:56:45 -0600

    php

---
+++ primera.php
@@ -0,0 +1,21 @@
+
+
+
+<?php
+ $p1 = $_GET["parametro1"]
+for ($i=0; $i <10 ; $i++) { 
+	# code..
+
+print("Hola PHP".$i."<br>");
+}
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title></title>
+</head>
+<body>
+	<?php print($p1); ?>
+</body>
+</html>
